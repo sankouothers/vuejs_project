cfe45621d94c626ad69c64dfd32d5dc4153da7f5
Pin Wang <pin.wang@ozstrategy.com>
2018-04-28 14:09:12 +0800

    如何引用 vuex store

--- mall/src/components/login.vue
+++ mall/src/components/login.vue
@@ -1,15 +1,19 @@
 <template>
   <h1>
+    //引用 vuex store 的方法
     {{$store.state.login.username}}
+    {{msg}}
   </h1>
 </template>
 
 <script>
+
 export default {
   name: 'login',
   data () {
     return {
-      // msg:$store.state.show
+      //引用 vuex store 的方法
+      msg: this.$store.state.login.username
     }
   }
 }
